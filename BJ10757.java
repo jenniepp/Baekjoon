7af50805bbe8a87b10f5adc6f15f6a2a840f3624
jenniepp <sj323130@nate.com>
2022-07-14 18:21:32 +0900

    Create BJ10757.java

---
+++ BJ10757.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BJ10757 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in); {
+			long A=sc.nextLong();
+			long B=sc.nextLong();
+			
+			long result=A+B;
+			System.out.println(result);
+		}
+
+	}
+
+}
